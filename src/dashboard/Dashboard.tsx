704144ce39987b536aff74427823b6cddf8f7eb7
eeddee <edward@edwardsharp.net>
2020-01-01 22:06:08 -0500

    tidy dashboard grid

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
@@ -13,38 +12,31 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2),
     minHeight: '100vh'
   },
-  item: {
-    // zIndex: 1
-  },
   paper: {
     padding: theme.spacing(2),
     display: 'flex',
     overflowX: 'hidden',
     flexDirection: 'column'
-  },
-  fixedHeight: {
-    height: 240
   }
 }))
 
 export default function Dashboard() {
   const classes = useStyles()
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight)
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6} className={classes.item}>
-          <Paper className={fixedHeightPaper}>
+        <Grid item xs={12} md={6}>
+          <Paper className={classes.paper}>
             <WholesaleOrders />
           </Paper>
         </Grid>
-        <Grid item xs={12} md={6} className={classes.item}>
-          <Paper className={fixedHeightPaper}>
+        <Grid item xs={12} md={6}>
+          <Paper className={classes.paper}>
             <Members />
           </Paper>
         </Grid>
-        <Grid item xs={12} className={classes.item}>
+        <Grid item xs={12}>
           <Paper className={classes.paper}>
             <Orders />
           </Paper>
