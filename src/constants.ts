9754aa0a6df0e0ae8b097ee915aa2ae648e866c5
eeddee <edward@edwardsharp.net>
2020-04-25 18:45:36 -0400

    update TAX_RATE

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const API_HOST =
     ? 'https://api.marshcoop.org'
     : 'http://localhost:3000'
 
-export const TAX_RATE = 0.06175
+export const TAX_RATE = 0.06391
 export const TAX_RATE_STRING = `${(TAX_RATE * 100).toFixed(3)}%`
 
 type OrderStatusLookup = { [key in OrderStatus]: string }
